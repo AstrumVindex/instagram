4563f4ce1ebf0b427dc24723cf6e56c89aebed64
Gautam kumar <123766944+AstrumVindex@users.noreply.github.com>
2025-03-02 20:04:22 +0530

    Update token main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 #Logger Setup
 logger = logging.getLogger(__name__)
 
-TOKEN = "YOUR_TOKEN_HERE"
+TOKEN = "1p4X6xHfarP5sDpGSwWSJUfjQrfpPvw7R"
 
 def download(update: Update, context: CallbackContext):
     message = update.effective_message
